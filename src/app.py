5e97701e44792e887583cb102bdc410548967680
ashwani1218 <ashwanipandey1218@gmail.com>
2020-12-07 16:26:53 +0530

    Added Api gateway support

--- src/app.py
+++ src/app.py
@@ -1,7 +1,19 @@
 import json
+import os
+
 
 print("loading function")
 
+
+def respond(err, res=None):
+    return {
+        'statusCode': '400' if err else '200',
+        'body': err.message if err else json.dumps(res),
+        'headers': {
+            'Content-Type': 'application/json',
+        },
+    }
+
+
 def lambda_handler(event, context):
-    return "hello world"
-    
+    return respond(None, res="Hello World")
