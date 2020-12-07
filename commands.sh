efd3aaaa15e0220cd827730c1c646d2a4ea200bd
ashwani1218 <ashwanipandey1218@gmail.com>
2020-12-07 15:45:30 +0530

    Initial commit

---
+++ commands.sh
@@ -0,0 +1,8 @@
+# Creating a s3 bucket
+aws s3 mb s3://ashwani-sam
+
+# Package Cloudformation
+aws cloudformation package --s3-bucket ashwani-sam --template-file template.yaml --output-template-file target/template-generated.yaml
+
+# Deploy
+aws cloudformation deploy --template-file target/template-generated.yaml --stack-name hello-world-sam --capabilities CAPABILITY_IAM 
---
+++ src/app.py
@@ -0,0 +1,7 @@
+import json
+
+print("loading function")
+
+def lambda_handler(event, context):
+    return "hello world"
+    
